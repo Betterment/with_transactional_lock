eb1671a42433e6495a301e03973725cdf51d37e8
Sam Moore <sam@betterment.com>
2016-06-01 14:07:55 -0400

    use a 128 length string for lock names

--- lib/generators/with_transactional_lock/install/templates/db/migrate/create_advisory_locks.rb
+++ lib/generators/with_transactional_lock/install/templates/db/migrate/create_advisory_locks.rb
@@ -1,7 +1,7 @@
 class CreateAdvisoryLocks < ActiveRecord::Migration
   def change
     create_table :advisory_locks, id: false do |t|
-      t.string :name, unique: true, null: false
+      t.string :name, unique: true, null: false, limit: 128
     end
   end
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -5,7 +5,7 @@ ActiveRecord::Schema.define do
 
   if ActiveRecord::Base.connection.adapter_name.downcase =~ /mysql/
     create_table "advisory_locks", id: false, force: :cascade do |t|
-      t.string "name", limit: 255
+      t.string "name", limit: 128
     end
     add_index "advisory_locks", ["name"], name: "index_advisory_locks_on_name", unique: true
   end
