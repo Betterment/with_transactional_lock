5a187ba0fc2dd6767ad920cb1d5b0bc30781a637
Sam Moore <sam@betterment.com>
2016-09-08 14:15:36 -0400

    use on-duplicate-key-update to improve concurrency

---
+++ lib/with_transactional_lock/my_sql_helper.rb
@@ -0,0 +1,13 @@
+module WithTransactionalLock
+  class MySqlHelper
+    def self.cleanup(klass = ActiveRecord::Base)
+      klass.connection_pool.with_connection do |conn|
+        target_count = conn.select_value('select count(1) from transactional_advisory_locks')
+        count = 0
+        until count >= target_count
+          count += conn.delete('delete from transactional_advisory_locks limit 1000')
+        end
+      end
+    end
+  end
+end
