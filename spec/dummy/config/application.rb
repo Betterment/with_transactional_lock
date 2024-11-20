b24203351907e013bfa81d1e4b9cc082646f5cf4
Ray Zane <raymondzane@gmail.com>
2024-11-20 10:23:35 -0500

    Fix specs

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,9 +4,6 @@ require File.expand_path('boot', __dir__)
 
 # Pick the frameworks you want:
 require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
